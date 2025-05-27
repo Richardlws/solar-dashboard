1f110aefba04526bc50c50290d295df030d7d440
Richardlws <liu.bing66066@hotmail.com>
2025-05-27 19:23:17 +0800

    number guess gaming

--- number guess gaming.py
+++ number guess gaming.py
@@ -13,8 +13,10 @@ else:
     quit()
 
 random_number = random.randint(0, top_of_range)
+guesses = 0
 ##print(random_number)
 while True:
+    guesses += 1
     user_guess = input("Make a guess: ")
     if user_guess.isdigit():
         user_guess = int(user_guess)
@@ -22,11 +24,13 @@ while True:
         print('Please type a number next time.')
         continue
 
-    if user_guess==random_number:
+    if user_guess == random_number:
         print("You got it!")
         break
     else:
-        print("You got it wrong!")
-
-
-
+        if user_guess>random_number:
+            print("You were above the number!")
+        else:
+            print("You were below the number!")
+        ##print("You got it wrong!")
+print("You got it in",guesses,"guesses")
