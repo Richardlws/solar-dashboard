c17c4e9e04417dec0958d220eb4d47d1095a80b4
Richardlws <liu.bing66066@hotmail.com>
2025-05-28 13:01:04 +0800

    number guess gaming

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -18,17 +18,17 @@ while True:
     print("Computer picked", computer_pick + ".")
     if user_input == computer_pick:
         print("Tie")
-    elif (user_input == "rock" and computer_pick == "scissors"):
+    elif user_input == "rock" and computer_pick == "scissors":
         print("You won!")
         user_wins += 1
-    elif(user_input == "paper" and computer_pick == "rock"):
+    elif user_input == "paper" and computer_pick == "rock":
         print("You won!")
         user_wins+=1
-    elif(user_input == "scissors" and computer_pick == "paper"):
+    elif user_input == "scissors" and computer_pick == "paper":
         print("You won!")
         user_wins+=1
     else:
-        print("You lose!")
+        print("You lost!")
         computer_wins+=1
 
 print("Goodbye!")
