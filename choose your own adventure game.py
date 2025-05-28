1a32d6a3339befe9f7352e39af1b09fcce1de18b
Richardlws <liu.bing66066@hotmail.com>
2025-05-28 19:15:17 +0800

    number guess gaming

---
+++ choose your own adventure game.py
@@ -0,0 +1,19 @@
+name = input("Type your name: ")
+print("Welcome",name,"to this adventure！")
+
+answer = input(
+      "You are on a dirt road,it has come to an end and you can go left or right.which way would you like to go? ").lower()
+
+if answer == "left":
+    answer = input("You come to a river,you can walk around it or swim across? Type walk to walk and swim to swim across: ")
+
+    if answer=="swim":
+        print()
+    elif answer=="walk":
+        print()
+    else:
+        print("Not a valid option. You lose")
+elif answer=="right":
+    print()
+else:
+    print("Not a valid option. You lose.")
