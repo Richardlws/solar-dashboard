5f7da8d972a1a19151f368a2c6b57d4f9e4eacbc
Richardlws <liu.bing66066@hotmail.com>
2025-05-29 08:09:43 +0800

    password manager

--- password manager.py
+++ password manager.py
@@ -2,15 +2,16 @@ master_pwd = input("What is the master password? ")
 
 
 def view():
-    pass
-
+    with open('password.txt', 'r') as f:
+        for line in f.readlines():
+            print(line.rstrip())
 
 def add():
     name = input('Account Name: ')
     pwd = input('Password: ')
 
-    with open('password.txt','a') as f:
-
+    with open('password.txt', 'a') as f:
+        f.write(name + "|" + pwd + "\n")
 
 
 while True:
