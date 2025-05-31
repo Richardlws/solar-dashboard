37be57f3cfbd68d4671156ab3f677ed04a5cffe2
Richardlws <liu.bing66066@hotmail.com>
2025-05-31 17:58:07 +0800

    PIG

--- PIG.py
+++ PIG.py
@@ -25,8 +25,8 @@ player_scores = [0 for _ in range(players)]
 while max(player_scores) < max_score:
 
     for player_idx in range(players):
-        print("Player number", player_idx + 1, "'s total score is:", player_scores[player_idx], "\n")
-        print("Player number", player_idx + 1, "turn has just started!\n")
+        print("Player number", player_idx + 1, "'s total score is:", player_scores[player_idx])
+        print("Player number", player_idx + 1, "turn has just started!")
         current_score = 0
 
         while True:
@@ -43,7 +43,7 @@ while max(player_scores) < max_score:
                 print("You rolled a ", value)
             print("This turn your score is：", current_score)
         player_scores[player_idx] += current_score
-        print("Player number", player_idx + 1, "'s total score is:", player_scores[player_idx], "\n")
+        print("Player number", player_idx + 1, "'s total score is:", player_scores[player_idx])
 
 
 for player_idx in range(players):
