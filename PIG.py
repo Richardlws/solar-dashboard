8772d0a8fa169f2e08369e5b51159877b0e93d8c
Richardlws <liu.bing66066@hotmail.com>
2025-05-31 17:01:19 +0800

    PIG

--- PIG.py
+++ PIG.py
@@ -1,9 +1,10 @@
 import random
 
+
 def roll():
-    min_value =1
-    max_value=6
-    roll=random.randint(min_value,max_value)
+    min_value = 1
+    max_value = 6
+    roll = random.randint(min_value, max_value)
     return roll
 
 
@@ -11,9 +12,33 @@ while True:
     players = input("Enter the number of players(2 - 4): ")
     if players.isdigit():
         players = int(players)
-        if 2<=players<=4:
+        if 2 <= players <= 4:
             break
         else:
             print("Must be between 2 - 4 players.")
     else:
         print("Invalid,try again.")
+
+max_score = 50
+player_score = [0 for _ in range(players)]
+
+while max(player_score)<max_score:
+    for player_idx in range(players):
+        current_score=0
+
+    while True:
+        should_roll = input("Would you like to roll(y)? ")
+        if should_roll.lower()!="y":
+            break
+
+        value = roll()
+        if value == 1:
+            print("You rolled a 1! Turn done!")
+            break
+        else:
+            current_score+=value
+            print("You rolled a ",value)
+
+        print("Your score is：", current_score)
+
+    player_score[player_idx]+=current_score
